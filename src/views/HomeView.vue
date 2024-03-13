110dd76a67cc4d841cf0a4def9b1f794fd30cbfe
ChristophLasinger <lasingerch@gmail.com>
2024-03-13 15:04:38 +0100

    add very basic homepage and remove template stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+const project_name = 'Project T'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="headline">Welcome to {{ project_name }}</h1>
+
+  <button class="link">New Game</button>
+  <button class="link">Load Save</button>
 </template>
+
+<style>
+.headline {
+  color: red;
+  font-weight: bold;
+  font-size: 4em;
+  margin-top: 5rem;
+}
+
+.link {
+  color: hsla(160, 100%, 37%, 1);
+  background: transparent;
+  border: none;
+  margin: 7rem;
+  font-size: 2em;
+}
+
+.link:hover {
+  background-color: hsla(160, 100%, 37%, 1);
+  color: white;
+}
+</style>
